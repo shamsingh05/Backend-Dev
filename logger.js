1ff129b42d09f3bf73dfedeff2ff8cf1a199a354
samsonremix17 <shambhavi.singh_cs23@gla.ac.in>
2026-01-16 14:38:52 +0530

    commands

--- logger.js
+++
@@ -1,9 +0,0 @@
-const fs=require("fs")
-
-function logActivity(message){
-const timestamp=new Date().toLocaleString();
-const logmessage=`${message} -${timestamp}\n`;
-fs.appendFileSync("activity.log",logmessage);
-}
-
-module.exports=logActivity;
