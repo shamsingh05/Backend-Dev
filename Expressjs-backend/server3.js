02f18a80b2966c38ad8fd371a46a4c1390ff9047
samsonremix17 <shambhavi.singh_cs23@gla.ac.in>
2026-02-12 15:29:16 +0530

    Created html form to submit details

--- Expressjs-backend/server3.js
+++ Expressjs-backend/server3.js
@@ -3,8 +3,10 @@ const app=express();
 const fs=require("fs").promises;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.use(express.urlencoded({extended: true}))
 const readStudentsFromFile = async ()=>{
     const data = await fs.readFile("./students.json", "utf-8");
     return JSON.parse(data || "[]");
@@ -20,7 +22,20 @@ app.get("/", async(req,res)=>{
     res.render("form", {allStudents: fileData});
 })
 
+app.post("/students/register", async (req, res) => {
+    const { name, branch } = req.body;
+
+    const students = await readStudentsFromFile();
+
+    students.push({ name, branch });
+
+    await writeStudentsFromFile(students);
+
+
+    res.redirect("/");   
+});
+
 const PORT= 8000;
 app.listen(PORT, () => {
-  console.log("Server is listening on port:8000");
+  console.log(`Server is listening on port: ${PORT}`);
 });
